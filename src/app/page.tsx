f9fb4d7167b8c439ae6420e61f2450eb2014323e
Armadon <movsisyan191287@gmail.com>
2025-02-13 13:50:45 +0400

    Added zustand for category focus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Container, Filters, Title, TopBar } from "@/components/shared";
+import { ProductsGroupList } from "@/components/shared/products-group-list";
 
 export default function Home() {
   return (
@@ -8,13 +9,133 @@ export default function Home() {
       </Container>
       <TopBar />
       <Container className="mt-10 pb-14">
-        <div className="flex gab-[60px]">
+        <div className="flex gap-[80px]">
+          {/* Filters */}
           <div className="w-[250px]">
             <Filters />
           </div>
+          {/* Product list */}
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              List pizza
+              <ProductsGroupList
+                title="Pizza"
+                items={[
+                  {
+                  id: 1,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+
+                {
+                  id: 2,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+
+                {
+                  id: 3,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+
+                {
+                  id: 4,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+                {
+                  id: 5,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+                {
+                  id: 6,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+                {
+                  id: 7,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+                
+              ]}
+                categoryId={1}
+              />
+
+<ProductsGroupList
+                title="Combo"
+                items={[
+                  {
+                  id: 1,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+
+                {
+                  id: 2,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+
+                {
+                  id: 3,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+
+                {
+                  id: 4,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+                {
+                  id: 5,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+                {
+                  id: 6,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+                {
+                  id: 7,
+                  name: "Peperoni",
+                  imageUrl: "https://media.dodostatic.com/image/r:292x292/0194f612949f76c09578959b470ce492.avif",
+                  print: 10,
+                  items: [{ price: 10 }]
+                },
+                
+              ]}
+                categoryId={2}
+              />
             </div>
           </div>
         </div>
--- src/components/shared/categories.tsx
+++ src/components/shared/categories.tsx
@@ -1,25 +1,40 @@
+'use client';
+
 import { cn } from '@/lib/utils';
+import { useCategoryStore } from '@/store/category';
 import React from 'react';
 
 interface Props {
     className?: string;
 }
 
-const cats = ["Pizza", "Combo", "Snacks", "Cocktails", "Coffe", "Drinks", "Dessert"];
-const activeIndex = 0;
+
+const cats = [
+    { id:1, name:"Pizza"},
+    { id:2, name:"Combo"},
+    { id:3, name:"Snacks"},
+    { id:4, name:"Cocktails"},
+    { id:5, name:"Coffe"},
+    { id:6, name:"Drinks"},
+    { id:7, name:"Dessert"},
+];
 
 export const Categories: React.FC<Props> = ({ className }) => {
+
+    const categoryActiveId = useCategoryStore((state) => state.activeId);
+
     return (
         <div className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}>
             {
-                cats.map((cat, index) => (
+                cats.map(({name, id}, index) => (
                     <a className={cn(
                         "flex items-center font-bold h-11 rounded-2xl px-5",
-                        activeIndex === index && "bg-white shadow-md shadow-gray-200 text-primary"
+                        categoryActiveId === id && "bg-white shadow-md shadow-gray-200 text-primary"
                     )}
+                    href={`/#${name}`}
                         key={index}>
                         <button>
-                            {cat}
+                            {name}
                         </button>
                     </a>
                 ))
