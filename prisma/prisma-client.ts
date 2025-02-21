90cd8c5d671e07059fce2ba26876b2bcde470d80
Armadon <movsisyan191287@gmail.com>
2025-02-21 11:51:01 +0400

    Added seed.ts constants.ts: MOdified scema.prisma: Removed coments from prisma-client

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -14,28 +14,3 @@ declare global {
 export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
-
-
-// import {PrismaClient} from "@prisma/client";
-
-// import Adapters from "next-auth/adapters";
-
-// const prismaClientPropertyName = `__prevent-name-collision__prisma`;
-// type GlobalThisWithPrismaClient = typeof globalThis & {
-// 	[prismaClientPropertyName]: PrismaClient;
-// };
-
-// const getPrismaClient = () => {
-// 	if (process.env.NODE_ENV === `production`) {
-// 		return new PrismaClient();
-// 	} else {
-// 		const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
-// 		if (!newGlobalThis[prismaClientPropertyName]) {
-// 			newGlobalThis[prismaClientPropertyName] = new PrismaClient();
-// 		}
-// 		return newGlobalThis[prismaClientPropertyName];
-// 	}
-// };
-// const prisma= getPrismaClient();
-
-// export default Adapters.Prisma.Adapter({prisma});
